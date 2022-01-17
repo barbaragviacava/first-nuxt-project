726c8c882c02248d5798c3a6488fdb2a6b4c0cd5
Barbara Garcia <barbara.gviacava@gmail.com>
2022-01-17 14:40:28 -0300

    Unificando as regras de validação para o update e o create

--- backend/app/app/Http/Requests/CategoriaRequest.php
+++ backend/app/app/Http/Requests/CategoriaRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
-class CategoriaStoreRequest extends FormRequest
+class CategoriaRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
