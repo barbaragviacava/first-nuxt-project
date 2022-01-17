11a476f8664c145ea205b0e3af2c9df6fd91fdc7
Barbara Garcia <barbara.gviacava@gmail.com>
2022-01-17 14:41:36 -0300

    Melhorias para os resources

--- backend/app/app/Providers/AppServiceProvider.php
+++ backend/app/app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +26,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        JsonResource::withoutWrapping();
+
+        // DB::listen(function ($query) {
+        //     Log::debug($query->sql);
+        //     Log::debug($query->bindings);
+        //     Log::debug($query->time);
+        // });
     }
 }
