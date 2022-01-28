a1eb934dde6c685e6a8fd0cadb1fd5733021a8a9
Barbara Garcia <barbara.gviacava@gmail.com>
2022-01-28 17:12:26 -0300

    Novas telas de produtos

---
+++ frontend/app/pages/produtos/form/_id.vue
@@ -0,0 +1,181 @@
+<template>
+    <div>
+
+        <BBreadcrumb class="float-xl-end">
+            <BBreadcrumbItem :to="{ name: 'dashboard' }">Início</BBreadcrumbItem>
+            <BBreadcrumbItem :to="{ name: 'produtos' }">{{PLURAL_NAME}}</BBreadcrumbItem>
+            <BBreadcrumbItem active>{{(isEdit ? 'Editando' : 'Criando') +' '+ SINGULAR_NAME}}</BBreadcrumbItem>
+        </BBreadcrumb>
+
+        <BaseTitle>
+            {{PLURAL_NAME}}
+            <small>{{isEdit ? 'Editando' : 'Criando uma nova'}} {{SINGULAR_NAME | lower}}</small>
+        </BaseTitle>
+
+        <BasePanel>
+            <template #body>
+                <ValidationObserver ref="form" v-slot="{ passes }">
+                    <form method="POST" @submit.prevent="passes(onSubmit)">
+                        <fieldset>
+
+                            <BRow>
+                                <BCol>
+                                    <ValidationProvider v-slot="{ errors, classes }" vid="nome" :name="labels.nome" rules="required">
+                                        <div class="mb-3">
+                                            <label class="form-label required" for="nome" :class="{'vee-error' : $validationHelper.hasFieldError(errors)}">{{labels.nome}}</label>
+                                            <BaseInput id="nome" v-model.trim="registro.nome" :placeholder="labels.nome" :readonly="isLoading" :class="classes" />
+                                            <InputErrorsList :errors="errors" />
+                                        </div>
+                                    </ValidationProvider>
+                                </BCol>
+                                <BCol>
+                                    <ValidationProvider v-slot="{ errors, classes }" vid="categoria_id" :name="labels.categoria_id" rules="required">
+                                        <div class="mb-3">
+                                            <label class="form-label required" for="categoria_id" :class="{'vee-error' : $validationHelper.hasFieldError(errors)}">{{labels.categoria_id}}</label>
+                                            <SelectInfiniteCategoria
+                                                v-model="registro.categoria"
+                                                input-id="categoria_id"
+                                                :class="classes"
+                                                :placeholder="labels.categoria_id"
+                                                :disabled="isLoading"
+                                            />
+                                            <InputErrorsList :errors="errors" />
+                                        </div>
+                                    </ValidationProvider>
+                                </BCol>
+                            </BRow>
+
+                            <ValidationProvider v-show="!isEdit" v-slot="{ errors }" :name="labels.active" rules="required">
+                                <label class="form-label col-form-label col-md-3">Criar a {{SINGULAR_NAME | lower}} como ativa?</label>
+                                <div class="form-check form-switch">
+                                    <input id="active" v-model="registro.active" class="form-check-input" type="checkbox" />
+                                    <label class="form-check-label" for="active">Sim</label>
+                                </div>
+                                <small class="text-muted">Você pode alterar a situação da {{SINGULAR_NAME | lower}} a qualquer momento</small>
+                                <InputErrorsList :errors="errors" />
+                            </ValidationProvider>
+
+                            <hr />
+
+                            <BaseButton type="submit" class="btn-purple w-100px" :loading="isLoading">Salvar</BaseButton>
+                            <BaseButton type="button" class="btn-default w-100px" title="Você será redirecionado para a listagem" @click="voltar">Voltar</BaseButton>
+
+                        </fieldset>
+                    </form>
+                </ValidationObserver>
+
+            </template>
+        </BasePanel>
+
+    </div>
+</template>
+
+<script>
+import { ValidationProvider, ValidationObserver } from "vee-validate";
+import { PLURAL_NAME, SINGULAR_NAME } from '~/repositories/ProdutoRepository'
+
+export default {
+    components: {
+        ValidationObserver,
+        ValidationProvider
+    },
+    async asyncData({ params, $repository, error, $errorHandler }) {
+        try {
+            if (params.id > 0) {
+                const registro = await $repository.produtos.show(params.id)
+                return { registro }
+            }
+        } catch(errorException) {
+            const errorResponse = $errorHandler.setAndParse(errorException)
+
+            error({ statusCode: errorResponse.status, message: errorResponse.message })
+        }
+        return {}
+    },
+    data() {
+        return {
+            PLURAL_NAME,
+            SINGULAR_NAME,
+            labels: {
+                nome: 'Nome',
+                categoria_id: `Categoria`,
+                active: 'Situação',
+            },
+            registro: {
+                active: true
+            }
+        }
+    },
+    head() {
+        return {
+            title: (this.isEdit ? 'Editando' : 'Criando') + ' '+ SINGULAR_NAME,
+        }
+    },
+    computed: {
+        isLoading() {
+            return this.$coreLoading.isActive();
+        },
+        isEdit() {
+            return this.$route.params.id > 0
+        },
+    },
+    methods: {
+
+        onSubmit() {
+            if (this.isEdit) {
+                this.update()
+            } else {
+                this.store()
+            }
+        },
+
+        async store() {
+            try {
+
+                this.registro.categoria_id = (this.registro.categoria != null ? this.registro.categoria.id : null)
+
+                const registroNovo = await this.$repository.produtos.create(this.registro);
+
+                this.$toast.success('Criada com sucesso! Você será redirecionado para a tela de edição.')
+
+                this.$router.push({ name: 'produtos-form-id', params: {id : registroNovo.id} })
+
+            } catch (error) {
+                const errorInfo = this.$errorHandler.setAndParse(error)
+
+                if (errorInfo.status == 422) {
+                    this.$refs.form.setErrors(this.$errorHandler.get());
+                } else {
+                    this.$toast.error(errorInfo.message)
+                }
+            }
+        },
+
+        async update() {
+            try {
+
+                this.registro.categoria_id = (this.registro.categoria != null ? this.registro.categoria.id : null)
+
+                const registroAtualizado = await this.$repository.produtos.update(this.registro.id, this.registro);
+
+                this.$toast.success('Atualizada com sucesso!')
+
+                this.registro = registroAtualizado
+
+            } catch (error) {
+                const errorInfo = this.$errorHandler.setAndParse(error)
+
+                if (errorInfo.status == 422) {
+                    this.$refs.form.setErrors(this.$errorHandler.get());
+                } else {
+                    this.$toast.error(errorInfo.message)
+                }
+            }
+        },
+
+        voltar() {
+            this.$router.push({ name: 'produtos' })
+        }
+    }
+}
+</script>
--- frontend/app/pages/produtos/index.vue
+++ frontend/app/pages/produtos/index.vue
@@ -1,5 +1,308 @@
 <template>
     <div>
-        Produtos
+
+        <BBreadcrumb class="float-xl-end">
+            <BBreadcrumbItem :to="{ name: 'dashboard' }">Início</BBreadcrumbItem>
+            <BBreadcrumbItem active>{{ PLURAL_NAME }}</BBreadcrumbItem>
+        </BBreadcrumb>
+
+        <BaseTitle>
+            {{ PLURAL_NAME }}
+            <small>Gerenciamento de {{ PLURAL_NAME }}</small>
+        </BaseTitle>
+
+        <BasePanel>
+
+            <template #body>
+
+                <BRow class="my-2">
+                    <BCol cols="12" md="3" class="me-2 mb-2">
+                        <BInputGroup>
+                            <BInputGroupText class="bg-primary text-light border-0"><fa icon="search" /></BInputGroupText>
+                            <input v-model.lazy="filtros.nome" type="text" class="form-control" placeholder="Pesquise pelo nome aqui" :readonly="isLoading">
+                        </BInputGroup>
+                    </BCol>
+                    <BCol cols="6" md="4" class="mb-2">
+                        <BDropdown right no-flip :variant="filtros.active != '' ? 'outline-success' : 'white'" menu-class="w-300px" :disabled="isLoading">
+                            <template #button-content>
+                                <BIcon :icon="filtros.active != '' ? 'funnel-fill' : 'funnel'" class="me-2" /> Mais Filtros
+                            </template>
+                            <BDropdownForm>
+                                <fieldset>
+                                    <div class="mb-3">
+                                        <label class="form-label">Situação</label>
+                                        <div class="form-check mb-2">
+                                            <input id="filtro-active-1" v-model="filtros.active" value="" class="form-check-input" type="radio" name="active" />
+                                            <label class="form-check-label" for="filtro-active-1">Todas</label>
+                                        </div>
+                                        <div class="form-check mb-2">
+                                            <input id="filtro-active-2" v-model="filtros.active" value="sim" class="form-check-input" type="radio" name="active" />
+                                            <label class="form-check-label" for="filtro-active-2">Apenas ativas</label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input id="filtro-active-3" v-model="filtros.active" value="nao" class="form-check-input" type="radio" name="active" />
+                                            <label class="form-check-label" for="filtro-active-3">Apenas inativas</label>
+                                        </div>
+                                    </div>
+                                </fieldset>
+                            </BDropdownForm>
+                        </BDropdown>
+                    </BCol>
+                    <BCol class="text-end mb-2">
+                        <BaseButton class="btn-purple px-4" @click="criar">
+                            <fa icon="plus" class="me-2"/> Criar {{SINGULAR_NAME}}
+                        </BaseButton>
+                    </BCol>
+                </BRow>
+
+                <div v-if="checkedItemsCount > 0" class="d-flex justify-content-end my-1">
+                    <div class="me-2 pt-2">
+                        <h5>{{checkedItemsCount}} itens selecionados</h5>
+                    </div>
+                    <div class="ms-2">
+                        <BDropdown
+                            right
+                            text="O que fazer com os itens selecionados ?"
+                            variant="lime"
+                            split-variant="outline-lime"
+                            split
+                            boundary="window"
+                        >
+                            <BDropdownItem @click="edit(row.item)">
+                                Ativar todos
+                            </BDropdownItem>
+                            <BDropdownItem>
+                                Inativar todos
+                            </BDropdownItem>
+                            <BDropdownItem>
+                                Remover todos
+                            </BDropdownItem>
+                        </BDropdown>
+                    </div>
+                    <div class="ms-2">
+                        <BaseButton class="btn-outline-dark" @click="clearAllCheckedItems"><fa icon="minus-square" class="me-2" /> Limpar seleção de itens</BaseButton>
+                    </div>
+                </div>
+
+                <BTable
+                    ref="table"
+                    responsive
+                    :items="listar"
+                    :fields="fields"
+                    :filter="filtros"
+                    :current-page="meta.current_page"
+                    :per-page="meta.per_page"
+                    :sort-by.sync="ordernation.sortBy"
+                    :sort-desc.sync="ordernation.sortDesc"
+                >
+                    <template #table-busy>
+                        <div class="w-100 text-center">
+                            <LoaderDefault :size="70" color="gray" />
+                        </div>
+                    </template>
+
+                    <template #head(checkbox)="">
+                        <input v-model="isCheckAllChecked[meta.current_page]" class="form-check-input" type="checkbox" :disabled="isLoading" @click="checkAll">
+                    </template>
+
+                    <template #cell(checkbox)="row">
+                        <input v-model="checkedItems" class="form-check-input" type="checkbox" :value="row.item.id">
+                    </template>
+
+                    <template #cell(active)="row">
+                        <BaseBadge
+                            outline
+                            :class="{'border-green text-lime': row.item.active, 'border-secondary text-black-50' : !row.item.active}"
+                        >
+                            <fa :icon="[row.item.active ? 'fas' : 'far', 'circle']" class="me-1" />
+                            {{row.item.active | YesNo}}
+                        </BaseBadge>
+                    </template>
+
+                    <template #cell(actions)="row">
+                        <BDropdown right text="Ações" variant="default" no-caret boundary="window">
+                            <template #button-content>
+                                Ações <fa icon="angle-down" class="ms-1" />
+                            </template>
+                            <BDropdownItem @click="edit(row.item)">
+                                Editar
+                            </BDropdownItem>
+                            <BDropdownItem @click="toggleActive(row.item)">
+                                {{ row.item.active ? 'Inativar' : 'Ativar' }}
+                            </BDropdownItem>
+                            <BDropdownItem @click="remove(row.item)">
+                                Remover
+                            </BDropdownItem>
+                        </BDropdown>
+                    </template>
+                </BTable>
+
+                <div class="d-md-flex align-items-center">
+                    <div class="me-md-auto text-md-left text-center mb-2 mb-md-0">
+                        Mostrando {{meta.per_page}} do total de {{meta.total}} {{ PLURAL_NAME | lower }}
+                    </div>
+                    <ul class="pagination mb-0 justify-content-center">
+                        <BPagination
+                            v-model="meta.current_page"
+                            :total-rows="meta.total"
+                            :per-page="meta.per_page"
+                        />
+                    </ul>
+                </div>
+
+            </template>
+        </BasePanel>
     </div>
 </template>
+
+<script>
+import { PLURAL_NAME, SINGULAR_NAME } from '~/repositories/ProdutoRepository'
+
+export default {
+    data () {
+        return {
+            PLURAL_NAME,
+            SINGULAR_NAME,
+            fields: [
+                { key: 'checkbox', label: '', class: 'checkbox-column' },
+                { key: 'nome', label: 'Nome', sortable: true },
+                {
+                    key: 'categoria_id',
+                    label: 'Categoria',
+                    formatter(value, key, item) {
+                        return item.categoria.nome
+                    }
+                },
+                {
+                    key: 'active',
+                    label: 'Está ativa?',
+                    sortable: false
+                },
+                { key: 'actions', label: 'Ações', thClass: 'text-end', tdClass: 'text-end' }
+            ],
+            meta: {
+                total: 0,
+                current_page: 1,
+                per_page: 0,
+                from: 0,
+                to: 0,
+            },
+            ordernation: {
+                sortBy: 'nome',
+                sortDesc: false,
+            },
+            filtros: {
+                active: ''
+            },
+            isCheckAllChecked: {},
+            checkedItems: []
+        }
+    },
+    head: {
+        title: PLURAL_NAME
+    },
+    computed: {
+        isLoading() {
+            return this.$coreLoading.isActive();
+        },
+        localItemsId() {
+            return this.$refs.table.localItems.map(item => item.id)
+        },
+        checkedItemsCount() {
+            return this.checkedItems.length
+        }
+    },
+    methods: {
+        async listar() {
+            const { data, meta } = await this.$repository.produtos.list({
+                sortBy: this.ordernation.sortBy,
+                sortDirection: (this.ordernation.sortDesc ? 'desc' : 'asc'),
+                page: this.meta.current_page,
+                ...this.filtros
+            });
+
+            this.meta = meta
+
+            return data;
+        },
+
+        checkAll() {
+            if (this.isCheckAllChecked[this.meta.current_page]) {
+                this.checkedItems = this.checkedItems.filter(id => !this.localItemsId.includes(id));
+            } else {
+                this.checkedItems = this.checkedItems.concat(this.localItemsId)
+            }
+            // Para evitar ids repetidos
+            this.checkedItems = [...new Set(this.checkedItems)]
+        },
+
+        clearAllCheckedItems() {
+            this.checkedItems = []
+            this.isCheckAllChecked = {}
+        },
+
+        edit(item) {
+            this.$router.push({name: 'produtos-form-id', params: {id : item.id }});
+        },
+
+        toggleActive(item) {
+
+            const singular_name = SINGULAR_NAME.toLowerCase()
+
+            this.$swal.fire({
+                title: 'Tem certeza?',
+                html: `O ${singular_name} "${item.nome}" será <strong>${item.active ? 'in' : ''}ativado</strong>.`,
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: 'var(--bs-primary)',
+                confirmButtonText: 'Sim',
+                cancelButtonText: 'Não',
+            }).then(async (result) => {
+
+                if (result.isConfirmed) {
+                    try {
+                        await this.$repository.produtos.toggleActive(item.id)
+
+                        this.$refs.table.refresh();
+                    } catch (error) {
+                        const errorInfo = this.$errorHandler.setAndParse(error)
+
+                        this.$toast.error(errorInfo.message)
+                    }
+                }
+            })
+        },
+
+        remove(item) {
+            const singular_name = SINGULAR_NAME.toLowerCase()
+
+            this.$swal.fire({
+                title: '',
+                text: `Tem certeza que deseja remover o ${singular_name} "${item.nome}"?`,
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: 'var(--bs-primary)',
+                confirmButtonText: 'Sim, pode remover',
+                cancelButtonText: 'Não, não quero remover',
+            }).then(async (result) => {
+
+                if (result.isConfirmed) {
+                    try {
+                        await this.$repository.produtos.delete(item.id)
+
+                        this.$refs.table.refresh();
+                    } catch (error) {
+                        const errorInfo = this.$errorHandler.setAndParse(error)
+
+                        this.$toast.error(errorInfo.message)
+                    }
+                }
+            })
+        },
+
+        criar() {
+            this.$router.push({name: 'produtos-form-id'})
+        },
+    }
+}
+</script>
