2253266d8363093e6851aa5286518ff90e5b5df2
Barbara Garcia <barbara.gviacava@gmail.com>
2022-02-09 21:10:02 -0300

    Removendo chamada do loading do nuxt

--- frontend/app/core-admin/plugins/loading/Loading.js
+++ frontend/app/core-admin/plugins/loading/Loading.js
@@ -6,30 +6,14 @@ export default class Loading {
 
     start() {
         this.store.commit('app/loading/start')
-        this.#startNuxtLoading()
     }
 
     stop() {
         this.store.commit('app/loading/stop')
-        this.#stopNuxtLoading()
     }
 
     isActive() {
         return this.store.state.app.loading.isActive
-    }
-
-    #startNuxtLoading() {
-        if (!this.store._vm || !this.store._vm.$nuxt || !this.store._vm.$nuxt.$loading) {
-            return;
-        }
-        return this.store._vm.$nuxt.$loading.start();
-    }
-
-    #stopNuxtLoading() {
-        if (!this.store._vm || !this.store._vm.$nuxt || !this.store._vm.$nuxt.$loading) {
-            return;
-        }
-        return this.store._vm.$nuxt.$loading.finish();
     }
 
 }
